99a14c0f79fcf8454bba587d90bfb6f1e1635613
Branzia <visvanathan.tech@gmail.com>
2025-07-02 10:54:44 +0530

    Moved Filament Discovery paths to blueprint

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -23,22 +23,6 @@ class CmsServiceProvider extends BranziaServiceProvider implements ProvidesFilam
     {
         parent::register();
     }
-    public static function filamentDiscoveryPaths(): array
-    {
-        return [
-            'resources' => [
-                ['path' => __DIR__.'/Filament/Resources', 'namespace' => 'Branzia\\Cms\\Filament\\Resources'],
-            ],
-            'pages' => [
-                ['path' => __DIR__.'/Filament/Pages', 'namespace' => 'Branzia\\Cms\\Filament\\Pages'],
-            ],
-            'clusters' => [
-                ['path' => __DIR__.'/Filament/Clusters', 'namespace' => 'Branzia\\Cms\\Filament\\Clusters'],
-            ],
-            'widgets' => [
-                ['path' => __DIR__.'/Filament/Widgets', 'namespace' => 'Branzia\\Cms\\Filament\\Widgets'],
-            ],
-        ];
-    }
+
 }
 
